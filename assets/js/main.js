0d474af95d28489127a737cbd547fa903f4703ad
Saleh Hajiyev <60463562+salehden@users.noreply.github.com>
2020-06-12 03:32:16 +0400

    complete logo and human carousel (owl)

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,16 +3,56 @@ $(document).ready(function () {
     $('.dropdown').hover(function () {
         $(this).addClass("show");
         $(this).find(".dropdown-menu").addClass("show");
-        
+
     }, function () {
         $(this).removeClass("show");
         $(this).find(".dropdown-menu").removeClass("show");
     })
-    $(window).scroll(function(){
-        if($('html').scrollTop() > 150){
+    $(window).scroll(function () {
+        if ($('html').scrollTop() > 150) {
             $('.navbar').addClass('is-sticky')
-        }else{
+        } else {
             $('.navbar').removeClass('is-sticky')
+        }
+    })
+    $('#logo-carousel').owlCarousel({
+        rtl: true,
+        dots: false,
+        loop: true,
+        margin: 10,
+        autoplay: true,
+        autoplayTimeout: 3000,
+        autoplayHoverPause: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 3
+            },
+            1000: {
+                items: 5
+            }
+        }
+    })
+    $('#person-carousel').owlCarousel({
+        rtl: true,
+        dots: true,
+        loop: true,
+        margin: 10,
+        autoplay: true,
+        autoplayTimeout: 5000,
+        autoplayHoverPause: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 3
+            },
+            1000: {
+                items: 3
+            }
         }
     })
 })
